afcd70e2f5796b66f5b066bf2bc199a80fa998a6
HP <HP@LAPTOP-REMP9JTB.lan>
2023-08-13 17:05:47 +0700

    update

--- src/test/java/testcase/Search.java
+++ src/test/java/testcase/Search.java
@@ -1,12 +1,19 @@
 package testcase;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import base.BaseTest;
 
 public class Search extends BaseTest{
+	@Test
 	public static void SearchFunction() {
 		driver.findElement(By.id(prop.getProperty("Search_textbox"))).sendKeys("back");
 		driver.findElement(By.id(prop.getProperty("search_button"))).click();
+		String actual = driver.findElement(By.xpath(prop.getProperty("search_result_title"))).getText();
+		System.out.println(actual);
+		boolean actualResult = actual.contains("back");
+		Assert.assertTrue(actualResult);
 	}
 }
